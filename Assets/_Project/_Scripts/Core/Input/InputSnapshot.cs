eaf966349a4d5d88fd8c8ad4d3c040f4cde87de9
AlmikSS <ulmasoviskandar7@gmail.com>
2026-05-18 19:01:40 +0300

    added: input system

---
+++ Assets/_Project/_Scripts/Core/Input/InputSnapshot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Core.Input
+{
+    public struct InputSnapshot
+    {
+        public readonly Vector2 MoveInput;
+        public readonly Vector2 LookInput;
+        public readonly bool UseInput;
+
+        public InputSnapshot(Vector2 moveInput, Vector2 lookInput, bool useInput)
+        {
+            MoveInput = moveInput;
+            LookInput = lookInput;
+            UseInput = useInput;
+        }
+    }
+}
---
+++ Assets/_Project/_Scripts/Core/Input/InputSystem.cs
@@ -0,0 +1,56 @@
+﻿using Core.ServiceLocatorDI;
+using Core.TicksSystem;
+using UnityEngine;
+
+namespace Core.Input
+{
+    public class InputSystem : MonoBehaviour, ITickable, IService
+    {
+        private InputActions _inputActions;
+        private InputSnapshot _snapshot;
+        private bool _isUseInput;
+        private bool _isConstruct;
+        
+        public TickPhase Phase => TickPhase.InputPhase;
+        public InputSnapshot Snapshot => _snapshot;
+
+        public void Construct()
+        {
+            ServiceLocator.Register(this);
+            _inputActions = new InputActions();
+            _isConstruct = true;
+        }
+        
+        public void OnTick(float deltaTime)
+        {
+            var moveInput = _inputActions.Player.Move.ReadValue<Vector2>();
+            var lookInput = _inputActions.Player.Look.ReadValue<Vector2>();
+
+            _snapshot = new InputSnapshot(
+                moveInput,
+                lookInput,
+                _isUseInput);
+            
+            _isUseInput = false;
+        }
+
+        private void Update()
+        {
+            if (!_isConstruct) 
+                return;
+            
+            if (_inputActions.Player.Interact.WasPressedThisFrame())
+                _isUseInput = true;
+        }
+        
+        private void OnEnable()
+        {
+            _inputActions.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _inputActions.Disable();
+        }
+    }
+}
